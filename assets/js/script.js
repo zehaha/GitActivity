9bfb2defcc944fa2f50a84acdfca1c7bdc57bfdf
Dyosa Andaca <mariaaprilrose@gmail.com>
2015-03-13 08:39:37 +0800

    First Commit
    YOLO

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+$(function(){
+  $('.ui-section .logo').delay(300).animate({'opacity':'0.8'}, 800);
+  $('.ui-section h1').delay(1700).animate({'opacity':'1'}, 800);
+  $('.ui-section .description').delay(2200).animate({'opacity':'1'}, 800);
+});
